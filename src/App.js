497c31c64e9fdbe54383f175de9d2f00c33b0cdf
farhanazam41 <39867162+farhanazam41@users.noreply.github.com>
2020-07-26 15:26:31 -0700

    Added google fonts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import {createGlobalStyle } from 'styled-components';
 
+
+const GlobalStyle = createGlobalStyle`
+
+body {
+
+  font-family: 'Open Sans', sans-serif;
+
+}
+h1, h2, h3 {
+  font-family: 'Righteous', cursive;
+
+}
+
+`
 
 
 function App() {
   return (
-    <div >
-      <h2>Welcome to spicekorner</h2>
-    </div>
+    
+    <>
+      <GlobalStyle/>
+      <h2>Spice Korner</h2>
+      <div>Welcopme</div>
+      
+    </>
+    
   );
 }
 
